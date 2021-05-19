907a57f49681fee0e18b4eacb2d341a13f7e8986
arozrai <72703446+arozrai@users.noreply.github.com>
2021-05-19 20:38:41 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,45 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import dictionary from './database'
 
 export default class HomeScreen extends React.Component {
-    getWord=(word)=>{
-        var seachKeyword=word.toLowerCase()
-        var url = "https://rupinwhitehatjr.github.io/dictionary/"+searchKeyword+".json"
-        return fetch(url)
-        .then((data)=>{
-            if(data.status===200){
-                return data.json()
-            }
-            else{
-                return null
-            }
-        })
-        .then((response)=>{
-            var responseObject = response
-
-            if(responseObject){
-                var wordData = responseObject.definitions[0]
-                var definition=wordData.description
-                var lexicalCategory=wordData.wordtype
-
-                this.setState({
-                    "word":this.state.text,
-                    "definition":definition,
-                    "lexicalCategory":lexicalCategory
-                })
-            }
-            else{
-                this.setState({
-                    "word":this.state.text,
-                    "definition":"Not Found"
-                })
-            }
-        })
+    constructor(){
+        super()
+        this.state={
+          word:'',
+          isSearchPressed:'',
+          text:'',
+          definition:'',
+          lexicalCategory:''
+        }
+    }
+    getWord=(text)=>{
+        var text = text.toLowerCase()
+        try{
+            var word = dictionary[text]["word"]
+            var lexicalCategory = dictionary[text]["lexicalCategory"]
+            var definition = dictionary[text]["definition"]
+            this.setState({
+                "word": word,
+                "lexicalCategory": lexicalCategory,
+                "definition": definition
+            })
+        }
+        catch(err){
+            alert("Sorry This word is not available for now")
+            this.setState({
+                'text':'',
+                'isSearchPressed':false
+            })
+        }
     }
   render(){
     return (
-        <View>
         <View style={styles.container}>
         <TextInput
             style={styles.inputBox}
@@ -59,20 +55,26 @@ export default class HomeScreen extends React.Component {
             style={styles.searchButton}
             onPress={()=>{
                 this.setState({isSearchPressed:true})
-                this.getWord(this.state.text)
+                this.getWord(word)
             }}
         />
         </View>
-
-        <View style={styles.detailsContiner}>
-        <Text style={styles.detailsTitle}>
-            Type:{" "}
-        </Text>
-        <Text style={{fontSize:18}}>
-            {this.state.lexicalCategory}
-        </Text>
-        </View>
-  
+    );
+  }
+  render(){
+      return(
+          <View style={styles.detailsContiner}>
+              <Text style={styles.detailsTitle}>
+                  Type:{" "}
+              </Text>
+              <Text style={{fontSize:18}}>
+                  {this.state.lexicalCategory}
+              </Text>
+          </View>
+      )
+  }
+    render(){
+        return(
             <View style={{flexDirection:'row',flexWrap:'wrap'}}>
                 <Text style={styles.detailsTitle}>
                     Definition:{" "}
@@ -81,7 +83,6 @@ export default class HomeScreen extends React.Component {
                     {this.state.definition}
                 </Text>
             </View>
-        </View>
         )
     }
 }
